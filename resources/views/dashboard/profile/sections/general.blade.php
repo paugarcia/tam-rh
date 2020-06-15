3ae3baadd87687fda89fb5bef39f5b12b4c87b53
Your Namegithub <github@youtterds.com>
2020-06-15 16:30:02 +0200

    Aqui tenés... wey

--- resources/views/dashboard/profile/sections/general.blade.php
+++ resources/views/dashboard/profile/sections/general.blade.php
@@ -2,31 +2,8 @@
 <form action="">
     {{-- FORM @lang('nav-pills.profile-general') --}}
 
-
-<div class="container-fluid">
-
-    <div class="row">
-        <div class="col">
-            <h4>INFORMACIÓN DEL PUESTO DE TRABAJO</h4>
-        </div>
-    </div>
-   
-    <div class="form-row">      
-        <div class="form-group col-lg-4 col-md-4 col-sm-6">
-            <label for="office_id">Grajo Majo</label>
-            <select class="selectpicker form-control"
-                    data-live-search="true"
-                    data-live-search-placeholder="Búsqueda por nombre"
-                    data-live-search-style='startsWith'
-                    title="Seleccione"
-                    id="office_id">
-                <option>Oficina 1</option>
-                <option>oficina 2</option>
-                <option>Ooficina 3</option>
-            </select>  
-        </div> 
-
-    <div class="col-lg-5 col-md-4 col-sm-6">   
+<!-- Base
+<div class="col-lg-5 col-md-4 col-sm-6">   
         <div class="form-group">
             <select class="selectpicker"
                     data-live-search="true"
@@ -39,10 +16,80 @@
             </select>
         </div>
     </div>
-    <div class="col-lg-5 col-md-4 col-sm-6">   
-    One of three columns
+ -->
+<h4>INFORMACIÓ DEL PUESTO DE TRABAJO</h4>
+<div class="form-row">
+
+    <div class="form-group col-lg-4 col-md-6 col-sm-6">
+
+        <label for="office_id">Oficina</label>
+        
+        <div class="">   
+            <div class="form-group">
+                <select class="selectpicker"
+                        data-live-search="true"
+                        data-live-search-placeholder="Búsqueda por nombre"
+                        data-live-search-style='startsWith'
+                        title="Seleccione"
+                        id="office_id">
+                    <option>Oficina 1</option>
+                    <option>Oofincina 2</option>
+                    <option>Officina 3</option>
+                </select>
+            </div>
+        </div>
+
     </div>
+
+    <div class="form-group col-lg-4 col-md-6 col-sm-6">
+
+        <label for="departments_id">Departamento</label>
+        
+        <div class="">   
+            <div class="form-group">
+                <select class="selectpicker"
+                        data-live-search="true"
+                        data-live-search-placeholder="Búsqueda por nombre"
+                        data-live-search-style='startsWith'
+                        title="Seleccione"
+                        id="departments_id">
+                    <option>Departamento 1</option>
+                    <option>Deepartament 2</option>
+                    <option>Ddepartamento 3</option>
+                </select>
+            </div>
+        </div>
+        
     </div>
+
+    <div class="form-group col-lg-4 col-md-6 col-sm-6">
+
+        <label for="users_id">Reportar a...</label>
+        
+        <div class="">   
+            <div class="form-group">
+                <select class="selectpicker"
+                        data-live-search="true"
+                        data-live-search-placeholder="Búsqueda por nombre"
+                        data-live-search-style='startsWith'
+                        title="Seleccione"
+                        id="users_id">
+                    <option>Uusuari 1</option>
+                    <option>Ussuari 2</option>
+                    <option>UUssuari 3</option>
+                </select>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="progress axios">
+        <div class="indeterminate"></div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">GUARDAR</button>
 </div>
 
 </form>
