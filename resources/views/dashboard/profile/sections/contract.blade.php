0748386adb70cc35d992d10244aec522ed681257
Your Namegithub <github@youtterds.com>
2020-06-23 12:54:08 +0200

    Todos los archivos

--- resources/views/dashboard/profile/sections/contract.blade.php
+++ resources/views/dashboard/profile/sections/contract.blade.php
@@ -1,1 +1,113 @@
-FORM @lang('nav-pills.profile-contract')
+{{-- FORM @lang('nav-pills.profile-contract') --}}
+
+<form action="">
+    <h3>Información General</h3>
+
+    <div class="form-row">
+
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <label for="contracts_date_start">Inicio</label>
+            <input type="text" class="form-control" id="contracts_date_start" value="" placeholder="FECHA CHUNGA" required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <label for="contracts_date_ending">Final</label>
+            <input type="text" class="form-control" id="contracts_date_ending" value="" placeholder="FECHA CHUNGA" required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <label for="contract_title">Título</label>
+            <input type="text" class="form-control" id="contract_title" value="" placeholder="título..." required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="progress axios">
+            <div class="indeterminate"></div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">GUARDAR</button>
+    </div>
+</form>
+
+<form action="">
+    <h3>Horario laboral</h3>
+
+    <div class="form-row">
+
+        <div class="col-lg-1 col-md-1 col-sm-1">
+            <label for="contracts_hours">Horas</label>
+            <input type="text" class="form-control" id="contracts_hours" value="" placeholder="h..." required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2">
+            <label for="NO_ENCONTRADO_CAMPO_EQUIVALENTE">Unidad</label>
+            <select class="custom-select" id="NO_ENCONTRADO_CAMPO_EQUIVALENTE" required>
+                <option selected disabled value="" placeholder="">Día</option>
+                <option>Semana</option>
+            </select>
+            <div class="invalid-feedback">No has elegido opción</div>
+        </div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2">
+            <label for="NO_ENCONTRADO_CAMPO_EQUIVALENTE">Dias semana</label>
+            <input type="text" class="form-control" id="NO_ENCONTRADO_CAMPO_EQUIVALENTE" value="" placeholder="l,m,m,j,v,s,d..." required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+
+        <div class="col-lg-7 col-md-7 col-sm-7">
+            <label for="NO_ENCONTRADO_CAMPO_EQUIVALENTE">Resumen</label>
+            <input type="text" class="form-control" id="NO_ENCONTRADO_CAMPO_EQUIVALENTE" value="" placeholder="Resumen..." required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="progress axios">
+            <div class="indeterminate"></div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">GUARDAR</button>
+    </div>
+</form>
+
+<form action="">
+    <h3>Salario fijo</h3>
+
+    <div class="form-row">
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <label for="contract_periodicity_id">Tipo de retribución</label>
+            <select class="custom-select" id="contract_periodicity_id" required>
+                <option selected disabled value="">Anual</option>
+                <option>Mensual</option>
+                <option>Semanal</option>
+                <option>Por horas</option>
+            </select>
+            <div class="invalid-feedback">No has elegido opción</div>
+        </div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2">
+            <label for="contract_quantity">Cantidad</label>
+            <input type="text" class="form-control" id="contract_quantity" value="" placeholder="Qnt..." required>
+            <div class="valid-feedback">Valor aceptado!</div>
+        </div>
+
+        <div class="col-lg-7 col-md-7 col-sm-7"></div>
+    </div>
+
+    <div class="form-row">
+        <div class="progress axios">
+            <div class="indeterminate"></div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">GUARDAR</button>
+    </div>
+</form>
+
+
+
+
--- resources/views/dashboard/profile/sections/personal.blade.php
+++ resources/views/dashboard/profile/sections/personal.blade.php
@@ -7,19 +7,19 @@
 
     <div class="col-lg-4 col-md-4 col-sm-4">
         <label for="user_name">Nombre</label>
-        <input type="text" class="form-control" id="user_name" value="Nombre..." required>
+        <input type="text" class="form-control" id="user_name" value="" placeholder="Nombre..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <label for="user_surname">Apellidos</label>
-        <input type="text" class="form-control" id="user_surname" value="Apellidos..." required>
+        <input type="text" class="form-control" id="user_surname" value="" placeholder="Apellidos..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
     <div class="col-lg-2 col-md-2 col-sm-2">
         <label for="user_birthdate">Nacimiento</label>
-        <input type="text" class="form-control" id="user_birthdate" value="NACIMIENTO CHUNGO" required>
+        <input type="text" class="form-control" id="user_birthdate" value="" placeholder="NACIMIENTO CHUNGO" required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
@@ -30,7 +30,7 @@
     <div class="col-lg-3 col-md-3 col-sm-3">
         <label for="gender_id">Género</label>
         <select class="custom-select" id="gender_id" required>
-            <option selected disabled value="">Femenino</option>
+            <option selected disabled value="" placeholder="">Femenino</option>
             <option>Masculino</option>
         </select>
         <div class="invalid-feedback">No has elegido opción</div>
@@ -58,7 +58,7 @@
 
     <div class="col-lg-2 col-md-2 col-sm-2">
         <label for="user_phone_number">Teléfono</label>
-        <input type="text" class="form-control" id="user_phone_number" value="Número" required>
+        <input type="text" class="form-control" id="user_phone_number" value="" placeholder="Número" required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
@@ -96,13 +96,13 @@
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <label for="user_nif_nie">Documento de identidad</label>
-        <input type="text" class="form-control" id="user_nif_nie" value="DNI/NIE" required>
+        <input type="text" class="form-control" id="user_nif_nie" value="" placeholder="DNI/NIE" required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <label for="user_social_security_number">Número de Seguridad Social</label>
-        <input type="text" class="form-control" id="user_social_security_number" value="Número de la seguridad social..." required>
+        <input type="text" class="form-control" id="user_social_security_number" value="" placeholder="Número de la seguridad social..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
@@ -129,13 +129,13 @@
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <label for="user_address">Dirección principal</label>
-        <input type="text" class="form-control" id="user_address" value="Dirección primaria..." required>
+        <input type="text" class="form-control" id="user_address" value="" placeholder="Dirección primaria..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6">
         <label for="user_address_secondary">Dirección secundaria</label>
-        <input type="text" class="form-control" id="user_address_secondary" value="Dirección secundaria..." required>
+        <input type="text" class="form-control" id="user_address_secondary" value="" placeholder="Dirección secundaria..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
@@ -143,42 +143,26 @@
 
 <div class="form-row">
 
-    <div class="col-lg-3 col-md-3 col-sm-3">
+    <div class="col-lg-2 col-md-2 col-sm-3">
         <label for="user_city">Ciudad</label>
-        <input type="text" class="form-control" id="user_city" value="Ciudad..." required>
+        <input type="text" class="form-control" id="user_city" value="" placeholder="Ciudad..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
-    <div class="col-lg-2 col-md-2 col-sm-2">
+    <div class="col-lg-1 col-md-1 col-sm-2">
         <label for="user_postal_code">CP</label>
-        <input type="text" class="form-control" id="user_postal_code" value="CP..." required>
+        <input type="text" class="form-control" id="user_postal_code" value="" placeholder="CP..." required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
-    <div class="col-lg-4 col-md-4 col-sm-4">
-        <label for="user_department_id">Estado/Provincia/Región</label>
-        <input type="text" class="form-control" id="user_department_id" value="Estado/Provincia/Región" required>
+    <div class="col-lg-3 col-md-3 col-sm-4">
+        <label for="user_state_province_region">Estado/Provincia/Región</label>
+        <input type="text" class="form-control" id="user_state_province_region" value="" placeholder="Estado/Provincia/Región" required>
         <div class="valid-feedback">Valor aceptado!</div>
     </div>
 
-
-
-
-
-
-
-
     <div class="col-lg-3 col-md-3 col-sm-3">
-        <label for="gender_id">Género</label>
-        <select class="custom-select" id="gender_id" required>
-            <option selected disabled value="">Femenino</option>
-            <option>Masculino</option>
-        </select>
-        <div class="invalid-feedback">No has elegido opción</div>
-    </div>
-
-    <div class="col-lg-3 col-md-3 col-sm-3">
-        <label for="country_id">Nacionalidad</label>
+        <label for="country_id">País</label>
         <div>
             <div class="form-group">
                 <select class="selectpicker"
@@ -188,33 +172,16 @@
                         title="Seleccione"
                         id="country_id">
                     <option>Moza en Bike</option>
-                    <option>Japonesa</option>
-                    <option>Etrusca</option>
-                    <option>Arapajoe profundo</option>
+                    <option>Turquia</option>
+                    <option>China</option>
+                    <option>San Marino</option>
                 </select>
             </div>
         </div>
         <div class="invalid-feedback">No has elegido opción</div>
     </div>
 
-    <div class="col-lg-2 col-md-2 col-sm-2">
-        <label for="user_phone_number">Teléfono</label>
-        <input type="text" class="form-control" id="user_phone_number" value="Número" required>
-        <div class="valid-feedback">Valor aceptado!</div>
-    </div>
-
-    <div class="col-lg-4 col-md-4 col-sm-4">
-        <label for="user_email">Correo</label>
-        <div class="input-group">
-            <div class="input-group-prepend">
-            <span class="input-group-text">@</span>
-        </div>
-        <input type="text" class="form-control rounded-right" id="user_email" required>
-        <div class="invalid-feedback">Please choose a username.</div>
-        </div>
-    </div>
-    
-
+    <div class="col-lg-3 col-md-3 col-sm-0"></div>
 </div>
 
 <div class="form-row">
@@ -227,3 +194,42 @@
 
 </form>
 
+<form action="">
+    {{-- FORM @lang('nav-pills.profile-general') --}}
+
+<h3>Contacto de emergencia</h3>
+
+<div class="form-row">
+
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <label for="emergency_contacts_name">Nombre y apellidos</label>
+        <input type="text" class="form-control" id="emergency_contacts_name" value="" placeholder="Contacto de emergencia..." required>
+        <div class="valid-feedback">Valor aceptado!</div>
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-3">
+        <label for="emergency_contacts_phone_number">1r Teléfono</label>
+        <input type="text" class="form-control" id="emergency_contacts_phone_number" value="" placeholder="Teléfono primario..." required>
+        <div class="valid-feedback">Valor aceptado!</div>
+    </div>
+
+    <div class="col-lg-3 col-md-3 col-sm-3">
+        <label for="emergency_contacts_phone_number_secondary">2n Teléfono</label>
+        <input type="text" class="form-control" id="emergency_contacts_phone_number_secondary" value="" placeholder="Teléfono secundario..." required>
+        <div class="valid-feedback">Valor aceptado!</div>
+    </div>
+
+
+
+</div>
+
+<div class="form-row">
+
+    <div class="progress axios">
+        <div class="indeterminate"></div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">GUARDAR</button>
+</div>
+
+</form>
