64eb9948872984ddcd6593878e6b00a7ca0be3a0
Your Namegithub <github@youtterds.com>
2020-06-23 17:53:07 +0200

    ria ria

--- resources/views/dashboard/profile/sections/documents.blade.php
+++ resources/views/dashboard/profile/sections/documents.blade.php
@@ -1,1 +1,24 @@
-FORM @lang('nav-pills.profile-documents')
+{{--FORM @lang('nav-pills.profile-documents')--}}
+
+<h5>Subir documento</h5>
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+  </div>
+  <div class="custom-file">
+    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+  </div>
+</div>
+
+
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action active">
+    Cras justo odio
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+  <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+  <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+</div>
